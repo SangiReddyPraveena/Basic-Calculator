3d6e9d8ecec85001f88e70487918d348398c4118
SangiReddyPraveena <136290215+SangiReddyPraveena@users.noreply.github.com>
2023-06-12 09:41:07 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Calci from '../src/calculator/components/Calci.js';
+
+function App() {
+  return (
+    <div className="App">
+       <Calci/>
+    </div>
+  );
+}
+
+export default App;
